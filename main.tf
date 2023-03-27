997c56923a1d4430b2fc11c8b9c2d3320a067db2
mettashalini89@gmail.com <CatMatDog@123>
2023-03-27 14:03:17 -0600

    environment with db and apps

--- main.tf
+++ main.tf
@@ -20,6 +20,33 @@ resource "aws_docdb_cluster_instance" "cluster_instance" {
   instance_class     = var.instance_class
 }
 
+resource "aws_security_group" "main" {
+  name        = "docdb-${var.env}"
+  description = "docdb-${var.env}"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "DOCDB"
+    from_port        = 27017
+    to_port          = 27017
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_subnets
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(
+    var.tags,
+    {Name = "docdb-${var.env}"}
+  )
+}
+
 resource "aws_docdb_subnet_group" "main" {
   name       = "${var.env}-docdb"
   subnet_ids = var.subnet_ids
@@ -47,4 +74,6 @@ resource "aws_ssm_parameter" "docdb_endpoint" {
   type  = "String"
   value = aws_docdb_cluster.main.endpoint
 }
+
+
 
